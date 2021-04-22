27f157df4781383215953fd9befb8e7f10050b1c
Darin Son <dson64@outlook.com>
2021-04-22 00:11:51 -0700

    first commit, set up html and js but error with accessing cross origin data

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Taking in Form Data
+const form = document.querySelector(`#ask-user-form`)
+
+let queries = {}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    queries = {
+        query_year: document.querySelector(`#year`).value,
+        query_round: document.querySelector(`#round`).value
+    }
+    console.log(queries.query_year)
+    console.log(queries.query_round)
+})
+
+// Data for Table
+const getData = async () => {
+    const url = `http://ergast.com/api/f1/${queries.query_year}/${queries.query_round}/driverstandings.json`
+    let response = await axios.get(url)
+    console.log(response)
+    return response.data
+}
+
+const createRow = (position, firstname, lastname, nationality, sponsor, points, driver_url) => {
+    const htmlRow = `<tr><th scope="row">${position}</th><td><a href=${driver_url}>${firstname} ${lastname}</a></td><td>${nationality}</td><td>${sponsor}</td><td>${points}</td></tr>`
+    document.querySelector(`.show-standings`).insertAdjacentHTML(`beforeend`, htmlRow)
+}
+
+const loadData = async () => {
+    const standings = await getData();
+    const Info = standings.MRData.StandingsTable.StandingsLists[0].DriverStandings
+    Info.forEach(element => createRow(
+        element.position,
+        element.Driver.givenName,
+        element.Driver.familyName,
+        element.Driver.nationality,
+        element.Constructors[0].name,
+        element.points,
+        element.Driver.url))
+}
+
