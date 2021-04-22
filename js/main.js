948e703faafaad37d395cbcf085d96115d1f94c1
Darin Son <dson64@outlook.com>
2021-04-22 16:18:45 -0700

    clears table b4 new query. msg for no query avail

--- js/main.js
+++ js/main.js
@@ -9,17 +9,14 @@ form.addEventListener('submit', (event) => {
         query_year: document.querySelector('#year').value,
         query_round: document.querySelector('#round').value
     }
-    console.log(queries.query_year)
-    console.log(queries.query_round)
 })
 
-// Data for Tableß
+// Data for Table
 const getData = async () => {
     let season = document.querySelector('#year').value;
     let round = document.querySelector('#round').value;
     const url = `http://ergast.com/api/f1/${season}/${round}/driverstandings.json`
     let response = await axios.get(url)
-    console.log(response)
     return response.data
 }
 
@@ -30,14 +27,20 @@ const createRow = (position, firstname, lastname, nationality, sponsor, points, 
 
 const loadData = async () => {
     const standings = await getData();
-    const Info = standings.MRData.StandingsTable.StandingsLists[0].DriverStandings
-    Info.forEach(element => createRow(
-        element.position,
-        element.Driver.givenName,
-        element.Driver.familyName,
-        element.Driver.nationality,
-        element.Constructors[0].name,
-        element.points,
-        element.Driver.url))
+    console.log(standings)
+    document.querySelector(`.show-standings`).innerHTML = ''
+    if (standings.MRData.StandingsTable.StandingsLists.length == 0) {
+        document.querySelector(`.show-standings`).insertAdjacentHTML(`beforeend`, '<p>Nothing to show here!</p>')
+    } else {
+        const Info = standings.MRData.StandingsTable.StandingsLists[0].DriverStandings
+        Info.forEach(element => createRow(
+            element.position,
+            element.Driver.givenName,
+            element.Driver.familyName,
+            element.Driver.nationality,
+            element.Constructors[0].name,
+            element.points,
+            element.Driver.url))
+    }
 }
 
